0487b435d93b789d702c15e86d811cfaa795dfe6
Morten Olav Hansen <mortenoh@gmail.com>
2016-01-18 17:09:18 +0700

    revno: 21007 message:   R 21774
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: 2.21
    timestamp: Mon 2016-01-18 17:09:18 +0700

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/ValidationUtils.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/ValidationUtils.java
@@ -448,7 +448,7 @@ public class ValidationUtils
             return null;
         }
 
-        if ( storedBy.length() > 31 )
+        if ( storedBy.length() > 255 )
         {
             return "stored_by_length_greater_than_max_length";
         }
