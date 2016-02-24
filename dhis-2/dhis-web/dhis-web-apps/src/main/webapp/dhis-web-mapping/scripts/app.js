560109f42a8b1b3b750aff0de43b4361c7829548
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-02-24 15:04:18 +0100

    revno: 21028 message:   GIS show radius numberfields for predefined legend sets.
    committer: Jan Henrik Overland <janhenrik.overland@gmail.com>
    branch nick: 2.21
    timestamp: Wed 2016-02-24 15:04:18 +0100

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-mapping/scripts/app.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-mapping/scripts/app.js
@@ -7480,14 +7480,23 @@ Ext.onReady( function() {
 		legendTypeToggler = function(legendType) {
 			if (legendType === 'automatic') {
 				methodPanel.show();
-				lowPanel.show();
-				highPanel.show();
+
+                colorLow.enable();
+                lowPanelLabel.update(GIS.i18n.low_color_size + ':');
+                colorHigh.enable();
+                highPanelLabel.update(GIS.i18n.high_color_size + ':');
+                
 				legendSet.hide();
+
 			}
 			else if (legendType === 'predefined') {
 				methodPanel.hide();
-				lowPanel.hide();
-				highPanel.hide();
+                
+                colorLow.disable();
+                lowPanelLabel.update(GIS.i18n.low_size + ':');
+                colorHigh.disable();
+                highPanelLabel.update(GIS.i18n.high_size + ':');
+                
 				legendSet.show();
 			}
 		};
@@ -8599,17 +8608,26 @@ Ext.onReady( function() {
 			]
 		});
 
+        lowPanelLabel = Ext.create('Ext.panel.Panel', {
+            html: GIS.i18n.low_color_size + ':',
+            width: 100,
+            style: 'padding: 4px 0 0 4px',
+            bodyStyle: 'border: 0 none'
+        });
+
+        highPanelLabel = Ext.create('Ext.panel.Panel', {
+            html: GIS.i18n.high_color_size + ':',
+            width: 100,
+            style: 'padding: 4px 0 0 4px',
+            bodyStyle: 'border: 0 none'
+        });
+
 		lowPanel = Ext.create('Ext.container.Container', {
 			layout: 'hbox',
             height: 25,
             bodyStyle: 'border: 0 none',
 			items: [
-				{
-					html: GIS.i18n.low_color_size + ':',
-					width: 100,
-					style: 'padding: 4px 0 0 4px',
-                    bodyStyle: 'border: 0 none'
-				},
+				lowPanelLabel,
 				colorLow,
 				radiusLow
 			]
@@ -8620,12 +8638,7 @@ Ext.onReady( function() {
             height: 25,
             bodyStyle: 'border: 0 none',
 			items: [
-				{
-					html: GIS.i18n.high_color_size + ':',
-					width: 100,
-					style: 'padding: 4px 0 0 4px',
-                    bodyStyle: 'border: 0 none'
-				},
+				highPanelLabel,
 				colorHigh,
 				radiusHigh
 			]
--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-mapping/scripts/custom.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-mapping/scripts/custom.js
@@ -6,6 +6,7 @@
 Ext.define('Ext.ux.button.ColorButton', {
 	extend: 'Ext.button.Button',
 	alias: 'widget.colorbutton',
+    disabledCls: 'gis-colorbutton-disabled',
 	width: 109,
 	height: 22,
 	defaultValue: null,
